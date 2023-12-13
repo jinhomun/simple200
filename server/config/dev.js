cac2f0b0f3b71781bfdeb6ea6a8e4fb9b039b1cb
jinhomun <answlsgh95@gmail.com>
2023-12-13 12:09:06 +0900

    게시글 끝

---
+++ server/config/dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongoURI: "mongodb+srv://answlsgh95:UGG7QMfWPOyW3UpV@cluster0.xlsy4ku.mongodb.net/reactBlog?retryWrites=true&w=majority",
+    access_key: "x5zdlyF7XQ5OktJB6sef",
+    secret_key: "5T6aNQTiHLThzv4v9l0r7DUStOqM4r4YPyojVWDn"
+}
